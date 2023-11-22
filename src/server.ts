44800770b2de47850ee999b3c8a73af5535a5432
sahik7 <sahikahsan7@gmail.com>
2023-11-22 23:24:34 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app"
+import mongoose from "mongoose";
+import config from "./config";
+const port = 5000
+
+// getting-started.js
+
+
+async function server() {
+    try {
+        await mongoose.connect(config.database_url_local);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+server().catch(err => console.log(err));
